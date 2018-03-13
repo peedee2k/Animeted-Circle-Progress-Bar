8a2c14d05a9455af285c493766aaf0ec8b1df643
peedee2k <peedee_2k@yahoo.com>
2018-03-13 00:58:55 -0500

    First Commit

--- Animeted Circle Progress Bar/ViewController.swift
+++ Animeted Circle Progress Bar/ViewController.swift
@@ -9,17 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let shapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //Circle Path
+        let center = view.center
+        let cerculerPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        //Let's make another view
+        
+        let myView: UIView = {
+           let inview = UIView()
+            inview.backgroundColor = UIColor.yellow
+           inview.translatesAutoresizingMaskIntoConstraints = false
+           return inview
+        }()
+        view.addSubview(myView)
+        myView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        myView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        myView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        let centerInView = myView.center
+        let runningCirclePath = CAShapeLayer()
+        let myViewCerclePath = UIBezierPath(arcCenter: centerInView, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2*CGFloat.pi, clockwise: true)
+        
+        runningCirclePath.path = myViewCerclePath.cgPath
+        runningCirclePath.fillColor = UIColor.clear.cgColor
+        
+        runningCirclePath.strokeColor = UIColor.lightGray.cgColor
+        runningCirclePath.lineWidth = 10
+       // runningCirclePath.lineCap = kCALineCapRound
+       // runningCirclePath.strokeEnd = 0
+        myView.layer.addSublayer(runningCirclePath)
+        
+        
+        // Lets start drawing circle
+        
+        
+        
+        // Create a Track for circle
+        let trackPath = CAShapeLayer()
+        
+       trackPath.path = cerculerPath.cgPath
+        trackPath.lineWidth = 10
+        trackPath.strokeColor = UIColor.lightGray.cgColor
+        trackPath.fillColor = UIColor.clear.cgColor
+        
+       
+        view.layer.addSublayer(trackPath)
+        
+//        let cerculerPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: -CGFloat.pi/2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        shapeLayer.path = cerculerPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.lineWidth = 10
+        shapeLayer.lineCap = kCALineCapRound
+        
+        shapeLayer.strokeEnd = 0
+        
+        view.layer.addSublayer(shapeLayer)
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func handleTap() {
+       let basicAnimaton = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimaton.toValue = 1
+        basicAnimaton.duration = 2
+        basicAnimaton.fillMode = kCAFillModeForwards
+        basicAnimaton.isRemovedOnCompletion = false
+        shapeLayer.add(basicAnimaton, forKey: "urSoBasic")
     }
-
-
+    
+    
 }
 
